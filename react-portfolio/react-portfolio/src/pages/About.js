402efa092bb04cd5695f907bf74a490372779b53
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Pro-6.local>
2023-03-16 14:02:10 -0700

    components/pages work

---
+++ react-portfolio/react-portfolio/src/pages/About.js
@@ -0,0 +1,37 @@
+function About() {
+    const styles = {
+        title: {
+            display: 'flex',
+            justifyContent: 'space-between',
+        },
+        portraitImg: {
+            float: 'left',
+            marginRight: '10px',
+
+        },
+    };
+
+    return (
+        <section className='bg-gray p-5' id='about'>
+            <section style={styles.title}>
+                <h1>About me</h1>
+            </section>
+            <section>
+                <img src={portrait} alt='self portrait' style={styles.portraitImg} />
+                <h3>Intro</h3>
+                <p>
+                Welcome to my portfolio. My name is Andrew and I love coding! I made this website for people to get to know a bit about me and see my work as I try navigate this new world I'm just starting off in. Please, help yourself and look through my portfolio and feel free to contact me if you wish.
+                </p>
+                <section style={{ display: 'flex'}}>
+                    <h3>Backstory</h3>
+                    <i>If you're interested</i>
+                </section>
+                <p>
+                    Hi
+                </p>
+            </section>
+        </section>
+    );
+}
+
+export default About;
---
+++ react-portfolio/react-portfolio/src/pages/Resume.js
@@ -0,0 +1,63 @@
+import resumePDF from '../assets/resume.pdf';
+
+function Resume() {
+  const styles = {
+    listStyle: {
+      listStyleType: 'circle',
+    },
+  };
+
+  const frontendProficiencies = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'jQuery',
+    'Responsive Design',
+    'React',
+    'Bootstrap',
+    'Handlebars',
+    'Local Storage, Session Storage, IndexedDB',
+  ];
+  const backendProficiencies = [
+    'APIs',
+    'Node',
+    'Express',
+    'MySQL, Sequelize',
+    'MongoDB, Mongoose',
+    'REST',
+  ];
+
+  return (
+    <section className="bg-gray p-5" id="resume">
+      <h2>Resume</h2>
+      <p>
+        Download my{' '}
+        <a href={resumePDF} download="Andrew's Resume">
+          resume
+        </a>
+      </p>
+      <article>
+        <h3>Front-end Proficiencies</h3>
+        <ul>
+          {frontendProficiencies.map((string) => (
+            <li style={styles.listStyle} key={string}>
+              {string}
+            </li>
+          ))}
+        </ul>
+      </article>
+      <article>
+        <h3>Back-end Proficiencies</h3>
+        <ul>
+          {backendProficiencies.map((string) => (
+            <li style={styles.listStyle} key={string}>
+              {string}
+            </li>
+          ))}
+        </ul>
+      </article>
+    </section>
+  );
+}
+
+export default Resume;
